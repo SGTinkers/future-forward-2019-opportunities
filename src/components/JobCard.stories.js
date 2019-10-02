ef868a34320636b3a27f1ff7f647caa68d19a466
Faruq Rasid <me@ruqqq.sg>
2019-10-02 23:01:01 +0800

    Show multiple job cards in a list

--- src/components/JobCard.stories.js
+++ src/components/JobCard.stories.js
@@ -1,8 +1,26 @@
 import React from "react";
 import JobCard from "./JobCard";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import Grid from "@material-ui/core/Grid";
 
 export default {
   title: "Job Card"
 };
 
 export const jobCard = () => <JobCard />;
+
+export const jobCardsInList = () => (
+  <Grid container spacing={2}>
+    <Grid item xs={12} md={12}>
+      <List>
+        <ListItem>
+          <JobCard />
+        </ListItem>
+        <ListItem>
+          <JobCard />
+        </ListItem>
+      </List>
+    </Grid>
+  </Grid>
+);
