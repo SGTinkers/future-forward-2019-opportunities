ab6d1016edef52b3c00819f4b29a753d52b262d4
Faruq Rasid <me@ruqqq.sg>
2019-10-03 22:11:17 +0800

    Reference extracted AppBar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React from "react";
-import logo from "./logo.svg";
 import { BrowserRouter } from "react-router-dom";
 import Home from "./scenes/Home";
 import Grid from "@material-ui/core/Grid";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import IconButton from "@material-ui/core/IconButton";
-import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
+import { FFAppBar } from "./components/FFAppBar";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -16,13 +12,6 @@ const useStyles = makeStyles(theme => ({
   menuButton: {
     marginRight: theme.spacing(2)
   },
-  title: {
-    flexGrow: 1,
-    display: "none",
-    [theme.breakpoints.up("sm")]: {
-      display: "block"
-    }
-  }
 }));
 
 function App() {
@@ -32,25 +21,12 @@ function App() {
     <Grid container justify="center">
       <Grid item xs={12}>
         <div className={classes.root}>
-          <AppBar position="static">
-            <Toolbar>
-              <IconButton
-                edge="start"
-                className={classes.menuButton}
-                color="inherit"
-              >
-                <img src={logo} alt="Future Forward" style={{ height: 36 }} />
-              </IconButton>
-              <Typography className={classes.title} variant="h6" noWrap>
-                Future Forward Opportunities
-              </Typography>
-            </Toolbar>
-          </AppBar>
+          <FFAppBar />
         </div>
       </Grid>
-      <Grid item>
+      <Grid item xs={12}>
         <BrowserRouter>
-          <Home />
+          <Home/>
         </BrowserRouter>
       </Grid>
     </Grid>
