b226dd456cf9b10b125b066f3adecb50d6eb30e6
Faruq Rasid <me@ruqqq.sg>
2019-10-03 21:20:38 +0800

    Add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+import Home from "./scenes/Home";
 
 function App() {
   return (
@@ -19,6 +21,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <BrowserRouter>
+        <Home/>
+      </BrowserRouter>
     </div>
   );
 }
