3c8a4962a202068fdb622dd2444e52fb0eea1219
Faruq Rasid <me@ruqqq.sg>
2019-10-03 22:40:12 +0800

    Add papaparse

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter } from "react-router-dom";
 import Home from "./scenes/Home";
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
 import { FFAppBar } from "./components/FFAppBar";
+import Papa from 'papaparse';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -16,6 +17,18 @@ const useStyles = makeStyles(theme => ({
 
 function App() {
   const classes = useStyles();
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch("/opportunities.csv");
+      const csvData = await response.text();
+
+      const data = Papa.parse(csvData, { header: true });
+      console.log(data);
+    }
+
+    fetchData();
+  });
 
   return (
     <Grid container justify="center">
