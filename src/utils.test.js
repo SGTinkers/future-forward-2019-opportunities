b76cd25d35a67492d65ee54086f071d13da4fb77
Faruq Rasid <me@ruqqq.sg>
2019-10-06 07:34:19 +0800

    Implement part time job parsing

--- src/utils.test.js
+++ src/utils.test.js
@@ -3,6 +3,8 @@ function parseJobResponse(jobResponse) {
     .map(jobType => {
       if (jobType === 'Full time job') {
         return parseFullTimeJobResponse(jobResponse);
+      } else if (jobType === 'Part time job') {
+        return parsePartTimeJobResponse(jobResponse);
       }
 
       return {
@@ -21,13 +23,23 @@ function parseFullTimeJobResponse(jobResponse) {
   }
 }
 
+function parsePartTimeJobResponse(jobResponse) {
+  return {
+    role: jobResponse['Potential part time role you will be offering:'],
+    company: jobResponse['Organisation Name'],
+    type: 'Part time',
+    hiringPeriod: jobResponse['When are you looking to start hiring part timers?'],
+    minQualification: jobResponse['Minimum Qualifications 2'],
+  }
+}
+
 describe("job response parsing", () => {
   it("parses full time job correctly", () => {
-    const jobResponse = createJobResponse();
+    const jobResponse = createFullTimeJobResponse();
 
     const jobs = parseJobResponse(jobResponse);
 
-    expect(jobs.length).toBe(2);
+    expect(jobs.length).toBe(1);
     expect(jobs[0]).toEqual({
       role: 'Developer',
       company: 'Circles.Life',
@@ -36,19 +48,80 @@ describe("job response parsing", () => {
       minQualification: 'Degree',
     });
   });
+
+  it("parses part time job correctly", () => {
+    const jobResponse = createPartTimeJobResponse();
+
+    const jobs = parseJobResponse(jobResponse);
+
+    expect(jobs.length).toBe(1);
+    expect(jobs[0]).toEqual({
+      role: 'Developer',
+      company: 'Circles.Life',
+      type: 'Part time',
+      hiringPeriod: 'Q2 2019',
+      minQualification: 'Degree',
+    });
+  });
 });
 
-function createJobResponse() {
+function createFullTimeJobResponse() {
   return {
     ID: "1",
     "Submission Date": "2019-09-17 22:40:05",
     "Organisation Name": "Circles.Life",
-    "Future Opportunities Available": "Full time job\nInternship",
+    "Future Opportunities Available": "Full time job",
     "When are you looking to start hiring full timers?": "Q2 2019",
     "Potential full time role you will be offering:": "Developer",
     "Minimum Qualifications": "Degree",
     "Job Description":
         "https://pages.circles.life/job-board/?gh_jid=4271631002",
+    "When are you looking to start hiring part timers?": "",
+    "Part time engagement estimated duration": "",
+    "Potential part time role you will be offering:": "",
+    "Minimum Qualifications 2": "",
+    "Job Description 2": "",
+    "When are you looking to start hiring interns?": "",
+    "Internship estimated duration": "",
+    "Potential internship role you will be offering:": "",
+    "Minimum Qualifications 3": "",
+    "Job Description 3": ""
+  };
+}
+
+function createPartTimeJobResponse() {
+  return {
+    ID: "1",
+    "Submission Date": "2019-09-17 22:40:05",
+    "Organisation Name": "Circles.Life",
+    "Future Opportunities Available": "Part time job",
+    "When are you looking to start hiring full timers?": "",
+    "Potential full time role you will be offering:": "",
+    "Minimum Qualifications": "",
+    "Job Description": "",
+    "When are you looking to start hiring part timers?": "Q2 2019",
+    "Part time engagement estimated duration": "1 Month",
+    "Potential part time role you will be offering:": "Developer",
+    "Minimum Qualifications 2": "Degree",
+    "Job Description 2": "https://pages.circles.life/job-board/",
+    "When are you looking to start hiring interns?": "",
+    "Internship estimated duration": "",
+    "Potential internship role you will be offering:": "",
+    "Minimum Qualifications 3": "",
+    "Job Description 3": ""
+  };
+}
+
+function createInternshipJobResponse() {
+  return {
+    ID: "1",
+    "Submission Date": "2019-09-17 22:40:05",
+    "Organisation Name": "Circles.Life",
+    "Future Opportunities Available": "Internship",
+    "When are you looking to start hiring full timers?": "",
+    "Potential full time role you will be offering:": "",
+    "Minimum Qualifications": "",
+    "Job Description": "",
     "When are you looking to start hiring part timers?": "",
     "Part time engagement estimated duration": "",
     "Potential part time role you will be offering:": "",
