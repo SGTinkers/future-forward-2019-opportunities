7e45a53740f1de6f5111e3122957a75a99de6227
Faruq Rasid <me@ruqqq.sg>
2019-10-06 19:53:52 +0800

    Add footer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,6 +9,7 @@ import {
   MuiThemeProvider
 } from "@material-ui/core/styles";
 import { FFAppBar } from "../FFAppBar";
+import { Typography } from "@material-ui/core";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -46,6 +47,9 @@ function App() {
           </Grid>
         </MuiThemeProvider>
       </BrowserRouter>
+      <div style={{ textAlign: 'center', color: '#deafad', fontStyle: 'italic' }}>
+        <Typography variant="subtitle2">&copy; 2019 Handcrafted by <a href="https://t.me/MSOCIETY" style={{ color: '#deafad' }}>MSOCIETY</a> in collaboration with <a href="http://www.thecodetteproject.com/" style={{ color: '#deafad' }}>The Codette Project.</a></Typography>
+      </div>
     </div>
   );
 }
