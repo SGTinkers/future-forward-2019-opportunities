54406626aa84108aff1fa805fe3622420071a816
saifulss <saifulss85@gmail.com>
2019-10-06 18:58:21 +0800

    Move image files to assets folder

--- src/components/FFAppBar.js
+++ src/components/FFAppBar.js
@@ -1,7 +1,7 @@
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
-import logo from "../ff-logo.png";
+import logo from "../assets/ff-logo.png";
 import Typography from "@material-ui/core/Typography";
 import React from "react";
 import { makeStyles } from "@material-ui/core";
