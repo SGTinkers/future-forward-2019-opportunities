25680ced89afe998f972afeab1af8e94e324cde4
saifulss <saifulss85@gmail.com>
2019-10-06 19:15:49 +0800

    Make router encompass both app bar and main content

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,56 +1,52 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import {BrowserRouter, Route} from "react-router-dom";
 import JobListingScene from "../../scenes/JobListingScene";
 import JobDetailScene from "../../scenes/JobDetailScene";
 import Grid from "@material-ui/core/Grid";
-import {
-  makeStyles,
-  createMuiTheme,
-  MuiThemeProvider
-} from "@material-ui/core/styles";
-import { FFAppBar } from "../FFAppBar";
+import {createMuiTheme, makeStyles, MuiThemeProvider} from "@material-ui/core/styles";
+import {FFAppBar} from "../FFAppBar";
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1
-  },
-  menuButton: {
-    marginRight: theme.spacing(2)
-  }
+    root: {
+        flexGrow: 1
+    },
+    menuButton: {
+        marginRight: theme.spacing(2)
+    }
 }));
 
 const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: '#deafad',
-      contrastText: '#fdfbf1',
-    },
-    secondary: {
-      main: '#374d5d',
-    },
-  }
+    palette: {
+        primary: {
+            main: '#deafad',
+            contrastText: '#fdfbf1',
+        },
+        secondary: {
+            main: '#374d5d',
+        },
+    }
 });
 
 function App() {
-  const classes = useStyles();
+    const classes = useStyles();
 
-  return (
-    <MuiThemeProvider theme={theme}>
-      <Grid container justify="center">
-        <Grid item xs={12}>
-          <div className={classes.root}>
-            <FFAppBar />
-          </div>
-        </Grid>
-        <Grid item xs={12}>
-          <BrowserRouter>
-            <Route path="/:id" component={JobDetailScene} />
-            <Route exact path="/" component={JobListingScene} />
-          </BrowserRouter>
-        </Grid>
-      </Grid>
-    </MuiThemeProvider>
-  );
+    return (
+        <BrowserRouter>
+            <MuiThemeProvider theme={theme}>
+                <Grid container justify="center">
+                    <Grid item xs={12}>
+                        <div className={classes.root}>
+                            <FFAppBar />
+                        </div>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Route path="/:id" component={JobDetailScene} />
+                        <Route exact path="/" component={JobListingScene} />
+                    </Grid>
+                </Grid>
+            </MuiThemeProvider>
+        </BrowserRouter>
+    );
 }
 
 export default App;
